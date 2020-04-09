f2e8f51b917b3dad0db879db7457a3fb0de40400
NumNine9 <57874698+NumNine9@users.noreply.github.com>
2020-04-10 00:45:39 +0200

    Add files via upload

---
+++ ggg.py
@@ -0,0 +1,15 @@
+#OBJECT ORIENTATED PROGRAMMING
+     
+class Dog:
+
+    def __init__(self):
+        self.nam
+        pass 
+
+    def meow(self,x):
+        return x + 1
+
+    def bark(self):   #this is a method ( .upper() )
+        print('bark')
+
+d = Dog()
